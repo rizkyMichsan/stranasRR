688734af3fa232758e89a0e13d14d6ac378e1210
rizkyMichsan <rizkymaulanaichsan@gmail.com>
2019-05-10 16:43:56 +0700

    memulai buat RestAPI

---
+++ application/controllers/api/Api_user.php
@@ -0,0 +1,40 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Api_User extends REST_Controller
+{
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->load->model('Front_model', 'front');
+    }
+    public function index_get()
+    {
+        $id = $this->get('id_article');
+
+        if ($id === null) {
+            $user = $this->front->all('article')->result_array();
+        } else {
+            $user = $this->front->where('article', array("id_article" => $id))->result_array();
+        }
+
+        if ($user) {
+            $this->response([
+                'status' => true,
+                'data' => $user
+            ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => FALSE,
+                'message' => 'users Not found'
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+}
